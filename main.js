012f9b747eb6185c8c34bcd2a6f54b49ce1745ba
Brittney Bannister <bbannister1127@icloud.com>
2020-06-29 18:45:22 -0400

    it works! now when player reached the end the browsers displays the winning message correctly

--- main.js
+++ main.js
@@ -29,18 +29,18 @@ function mazeDisplay(blueprint) {
                 container.appendChild(row)
                 cell.classList.add('wall')
                 row.appendChild(cell)
-            } else if (map[i][j] === " ") {
-                container.appendChild(row)
-                cell.classList.add('block')
-                row.appendChild(cell)
             } else if (map[i][j] === 'S') {
                 container.appendChild(row)
                 cell.classList.add('start')
                 row.appendChild(cell)
                 cell.id = 'start'
-            } else {
+            } else if (map[i][j] === 'F') {
                 container.appendChild(row)
                 cell.classList.add('finish')
+                row.appendChild(cell)
+            } else {
+                container.appendChild(row)
+                cell.classList.add('block')
                 row.appendChild(cell)
             }
         }
@@ -61,43 +61,44 @@ let playerRow = 9 //it's nine because my start position is (9,0)
 
 document.addEventListener("keydown", moveAround)
 
-function moveAround(evt) {
-    //TRYING TO LEAVE A 'TRAIL' BEHIND PLAYER...MAY HAVE TO COME BACK TO THIS...
-    // let trail = document.createElement('div')
-    // trail.classList.add('dot')
-    // playerRow.getElementById('dot').appendChild(trail)
-    if (evt.code === 'ArrowUp') {
-        if (map[playerRow - 1][playerColumn] !== 'W') {
-            playerTop -= 20
-            player.style.top = playerTop + 'px'
-            playerRow -= 1
-        }
-    } else if (evt.code === 'ArrowDown') {
-        if (map[playerRow + 1][playerColumn] !== 'W') {
-            playerTop += 20
-            player.style.top = playerTop + 'px'
-            playerRow += 1
-        }
-    } else if (evt.code === 'ArrowRight') {
-        if (map[playerRow][playerColumn + 1] !== 'W') {
-            playerLeft += 20
-            player.style.left = playerLeft + 'px'
-            playerColumn += 1
-        }
-    } else if (evt.code === 'ArrowLeft') {
-        if (playerRow <= 9 && playerColumn <= 0) {
-            return
-        } else if (map[playerRow][playerColumn - 1] !== 'W') {
-            playerLeft -= 20
-            player.style.left = playerLeft + 'px'
-            playerColumn -= 1
-        }
+//TRYING TO LEAVE A 'TRAIL' BEHIND PLAYER...MAY HAVE TO COME BACK TO THIS...
+// let trail = document.createElement('div')
+// trail.classList.add('dot')
+// playerRow.getElementById('dot').appendChild(trail)
 
+function moveAround(evt) {
+    if (evt.code === "ArrowUp") {
+        if (map[playerRow - 1][playerColumn] !== "W") {
+            playerTop -= 20;
+            player.style.top = playerTop + "px";
+            playerRow -= 1;
+        }
+    } else if (evt.code === "ArrowDown") {
+        if (map[playerRow + 1][playerColumn] !== "W") {
+            playerTop += 20;
+            player.style.top = playerTop + "px";
+            playerRow += 1;
+        }
+    } else if (evt.code === "ArrowLeft") {
+        if (playerRow <= 9 && playerColumn <= 0) {
+            return;
+        } else if (map[playerRow][playerColumn - 1] !== "W") {
+            playerLeft -= 20;
+            player.style.left = playerLeft + "px";
+            playerColumn -= 1;
+        }
+    } else if (evt.code === "ArrowRight") {
+        if (map[playerRow][playerColumn + 1] !== "W") {
+            playerLeft += 20;
+            player.style.left = playerLeft + "px";
+            playerColumn += 1;
+        }
         madeIt();
     }
 
+
     function madeIt() {
-        if (map[playerRow][playerColumn] == 'F') {
+        if (map[playerRow][playerColumn] === 'F') {
             let winMsg = document.createElement('div')
             winMsg.classList.add('win')
             document.body.appendChild(winMsg)
